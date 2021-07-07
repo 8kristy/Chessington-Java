21fd3c39bfd3195791d1f613d007dc893295235c
8kristy <8kristy@gmail.com>
2021-07-07 16:12:49 +0100

    Add king

--- src/main/java/training/chessington/model/pieces/King.java
+++ src/main/java/training/chessington/model/pieces/King.java
@@ -15,6 +15,20 @@ public class King extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> moves = new ArrayList<>();
+        int x = from.getCol();
+        int y = from.getRow();
+        for (int i =x-1; i < x+2; i++){
+            for (int j = y-1; j < y+2; j++){
+                Coordinates to = new Coordinates(i, j);
+                if (isValidMove(board, to) && !(i == x && j == y))
+                    moves.add(new Move(from, to));
+            }
+        }
+        return moves;
+    }
+
+    private boolean isValidMove(Board board, Coordinates to){
+        return checkBoardBounds(to) && (board.get(to) == null || board.get(to).getColour() != this.colour);
     }
 }
