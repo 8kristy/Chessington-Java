1f8fef605d17b68c646ae729f5406f5829c9f0f3
8kristy <8kristy@gmail.com>
2021-07-07 15:02:04 +0100

    Implement rook

--- src/main/java/training/chessington/model/pieces/Rook.java
+++ src/main/java/training/chessington/model/pieces/Rook.java
@@ -15,6 +15,25 @@ public class Rook extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> allowedMoves = new ArrayList<>();
+        addMovesInDirection(board, allowedMoves, from, 0, 1);
+        addMovesInDirection(board, allowedMoves, from, 1, 0);
+        addMovesInDirection(board, allowedMoves, from, 0, -1);
+        addMovesInDirection(board, allowedMoves, from, -1, 0);
+        return allowedMoves;
     }
+
+    private void addMovesInDirection(Board board, List<Move> allowedMoves, Coordinates from, int xDirection, int yDirection){
+        Coordinates to = from.plus(xDirection, yDirection);
+        while(isValidMove(board, to)) {
+            allowedMoves.add(new Move(from, to));
+            if(board.get(to) != null) break;
+            to = to.plus(xDirection, yDirection);
+        }
+    }
+
+    private boolean isValidMove(Board board, Coordinates to) {
+        return checkBoardBounds(to) && (board.get(to) == null || !board.get(to).getColour().equals(this.getColour()));
+    }
+
 }
